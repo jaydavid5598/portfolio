070871f5f50d0c6635c304a228491a35a260a83f
shinkiro55 <jaydavidrepo@gmail.com>
2020-08-17 14:05:29 +0800

    added welcome to

--- portfolio/src/components/Greet.js
+++ portfolio/src/components/Greet.js
@@ -5,7 +5,7 @@ export default class Greet extends Component {
   render() {
     return (
       <div className="greet">
-        <h1 className="greet-title">LANDIA PUB</h1>
+        <h1 className="greet-title">WELCOME TO LANDIA PUB</h1>
       </div>
     );
   }
